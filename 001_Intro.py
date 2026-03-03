b83d4d3f7de22cce89da7142af580bb40223baff
Vishesh Lamba <visheshlamba2005@gmail.com>
2026-03-03 09:13:37 +0530

    print func

--- 001_Intro.py
+++ 001_Intro.py
@@ -1,5 +1,14 @@
 # print(object1, object2, ..., sep=' ', end='\n') this is basic syntax for print function
 
-print("Hlw world") #string must be in quotes
-print('hi how r u')
-print(5*7)
+print("Hlw world","hehe",7*5, sep='-',end='.') # it will separate these by - and end function with .
+print('hi how r u') # by default the sep is a space and end is next line
+print(5*7) #35
+print("hello i m vishesh\nand i am a good boy") #\n is escape seq character, it will take u to next line
+    # print("Indentation error") it will give error and nothing will be executed 
+    
+''' 
+This is a multi line comment.
+hehehe hehehe heeheh 
+
+'''
+print("hello, \"i am iron man\" ") # \" gives " in the output
